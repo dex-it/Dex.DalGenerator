248329baafb7b4882fdd3d9b2a846c2254d0801f
Vitaliy Danilov <vitaliy931@gmail.com>
2021-07-08 14:50:21 +0300

    Nullable (#6)
    * rid of explicit Nullable<...>
    
    * CSharpCodeProvider

--- src/Dex.DalGenerator.Core/Extensions/TypeExtensions.cs
+++ src/Dex.DalGenerator.Core/Extensions/TypeExtensions.cs
@@ -1,7 +1,9 @@
 ﻿using System;
+using System.CodeDom;
 using System.Linq;
 using System.Reflection;
 using Dex.Ef.Contracts.Entities;
+using Microsoft.CSharp;
 
 namespace Dex.DalGenerator.Core.Extensions
 {
@@ -20,39 +22,55 @@ namespace Dex.DalGenerator.Core.Extensions
         public static string GetFriendlyName(this Type type, bool fullName = false)
         {
             if (type == typeof(int))
-                return "int";
+                return ToSimpleName(type);
             else if (type == typeof(short))
-                return "short";
+                return ToSimpleName(type);
             else if (type == typeof(byte))
-                return "byte";
+                return ToSimpleName(type);
             else if (type == typeof(bool))
-                return "bool";
+                return ToSimpleName(type);
             else if (type == typeof(long))
-                return "long";
+                return ToSimpleName(type);
             else if (type == typeof(float))
-                return "float";
+                return ToSimpleName(type);
             else if (type == typeof(double))
-                return "double";
+                return ToSimpleName(type);
             else if (type == typeof(decimal))
-                return "decimal";
+                return ToSimpleName(type);
             else if (type == typeof(string))
-                return "string";
+                return ToSimpleName(type);
+
+            var name = fullName
+                ? type.FullName
+                : type.Name;
+
+            if (type.GetTypeInfo().IsGenericType)
+            {
+                string args = string.Join(", ", type.GetTypeInfo().GetGenericArguments().Select(t => GetFriendlyName(t, fullName)).ToArray());
+
+                if (type.GetGenericTypeDefinition() == typeof(Nullable<>))
+                {
+                    return $"{args}?";
+                }
+                else
+                {
+                    return name.Split('`')[0] + "<" + args + ">";
+                }
+            }
             else
             {
-                var name = fullName ? type.FullName : type.Name;
-                if (type.GetTypeInfo().IsGenericType)
-                    return name.Split('`')[0] + "<" + string.Join(", ", type.GetTypeInfo().GetGenericArguments().Select(t => GetFriendlyName(t, fullName)).ToArray()) + ">";
-                else
-                    return name;
+                return name;
             }
         }
 
-
         public static bool IsNullable( this Type type)
         {
-            if (type == null) throw new ArgumentNullException(nameof(type));
+            if (type == null)
+            {
+                throw new ArgumentNullException(nameof(type));
+            }
 
-            return type.GetTypeInfo().IsGenericType && type.GetGenericTypeDefinition() == typeof (Nullable<>);
+            return type.GetTypeInfo().IsGenericType && type.GetGenericTypeDefinition() == typeof(Nullable<>);
         }
 
         public static Type MakeNullable(this Type type)
@@ -63,6 +81,16 @@ namespace Dex.DalGenerator.Core.Extensions
             }
 
             return typeof(Nullable<>).MakeGenericType(type);
+        }
+
+        private static string ToSimpleName(Type type)
+        {
+            using (var provider = new CSharpCodeProvider())
+            {
+                var typeRef = new CodeTypeReference(type);
+                string typeName = provider.GetTypeOutput(typeRef);
+                return typeName;
+            }
         }
     }
 }
